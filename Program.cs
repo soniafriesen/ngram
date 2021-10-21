4074e93608eb8254cb77f5df9d3f4ab93163379a
Sonia <s_friesen@fanshaweonline.ca>
2021-10-21 17:53:40 -0400

    Updated to match debug.txt examples

--- Program.cs
+++ Program.cs
@@ -32,92 +32,93 @@ namespace ngram
             string data = args[0]; //filename
 
                     
-            string testData = reader.readFile(data);
-            string newData = testData.Replace(".","");
+            string testData = reader.readFile(data);           
             List<string> nounsIndex = reader.readNouns("NounsIndex.txt"); // file is in bin/debug/net5.0
             List<string> nounsData = reader.readNouns("NounsData.txt"); // file is in bin/debug/net5.0
-            //string debugFile = Path.Combine(Environment.CurrentDirectory, @"", "debug.txt");
-
-            //test file reading
-            //reader.test(testData, nounsData, nounsIndex);
-
             //write the data file to the console
             Console.WriteLine();
-            Console.WriteLine(testData);
-            //string[] s = testData.Split(".");
-            //string sentence = s[0];
-            Console.WriteLine("\n");
-            Console.WriteLine(newData);
-            using(StreamWriter writer = new StreamWriter("debug.txt", false))
+            Console.WriteLine(testData);       
+            using (StreamWriter writer = new StreamWriter("debug.txt", false))
             {
-                writer.WriteLine(testData);               
-            }
-            using(StreamWriter writer = new StreamWriter("debug.txt", true))
-            {
-                writer.WriteLine();
-                writer.WriteLine(newData);                
+                writer.WriteLine(testData);
             }
 
             //convert lists into dictionaries
             Dictionary<string, string> nIndexs = reader.convertToDictionary(nounsIndex);
-            Dictionary<string,string> nData = reader.convertToDictionary(nounsData);
-
-            //dictionary fill test
-            //reader.testDictionary(nIndexs);
-            //reader.testDictionary(nData);
-
-            //n-gram 2,3,4 process
-
-            //n-gram level 2 process
-            Console.WriteLine();
-            Console.WriteLine("2 level n-gram");
-            using (StreamWriter writer = new StreamWriter("debug.txt", true))
+            Dictionary<string, string> nData = reader.convertToDictionary(nounsData);
+           
+            //get each sentence
+            string[] s = testData.Split(".");        
+            foreach (string sentence in s)
             {
-                writer.WriteLine();
-                writer.WriteLine("2 level n-gram");
-                writer.WriteLine();
-            }
-            Console.WriteLine();
-            string[] words = newData.Split(); //string of every sentence 
-            List<string> ngrams = reader.turnIntoNgram(2,words);
+                if (sentence == "") 
+                {
+                    break;
+                }
+                //newData = testData.Replace(".", "");
+                using (StreamWriter writer = new StreamWriter("debug.txt", true))
+                {
+                    writer.WriteLine();
+                    writer.WriteLine(sentence);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine(sentence);
+                Console.WriteLine();
+                //process the n-gram levels on each sentence
+                //n-gram 2,3,4 process
 
-            //searching for the indexs and definitions if the word has any
-            List<string> definitions = reader.getngram(ngrams, nIndexs, nData);
+                //n-gram level 2 process
+                Console.WriteLine();
+                Console.WriteLine("2 level n-gram");
+                using (StreamWriter writer = new StreamWriter("debug.txt", true))
+                {
+                    writer.WriteLine();
+                    writer.WriteLine("2 level n-gram");
+                    writer.WriteLine();
+                }
+                Console.WriteLine();
+                string literal = sentence.Trim();
+                string[] words = literal.Split(); //string of every sentence 
+                List<string> ngrams = reader.turnIntoNgram(2, words);
 
-            reader.print(definitions); //print to console
-            reader.writetofile(definitions); //write to debug.txt
-            Console.WriteLine();
+                //searching for the indexs and definitions if the word has any
+                List<string> definitions = reader.getngram(ngrams, nIndexs, nData);
 
-            //n-gram level 3 process
-        
-            Console.WriteLine("3 level n-gram");
-            Console.WriteLine();
-            using (StreamWriter writer = new StreamWriter("debug.txt", true))
-            {
-                writer.WriteLine();
-                writer.WriteLine("3 level n-gram");
-                writer.WriteLine();
-            }
-            ngrams = reader.turnIntoNgram(3, words);
-            definitions = reader.getngram(ngrams, nIndexs, nData);
-            reader.print(definitions);
-            reader.writetofile(definitions);
+                reader.print(definitions); //print to console
+                reader.writetofile(definitions); //write to debug.txt
+                Console.WriteLine();
 
-            //4 level ngram
-            Console.WriteLine();
-            Console.WriteLine("4 level n-gram");
-            Console.WriteLine();
-            using (StreamWriter writer = new StreamWriter("debug.txt", true))
-            {
-                writer.WriteLine();
-                writer.WriteLine("4 level n-gram");
-                writer.WriteLine();
-            }
-            ngrams = reader.turnIntoNgram(4, words);
-            definitions = reader.getngram(ngrams, nIndexs, nData);
-            reader.print(definitions);
-            reader.writetofile(definitions);
+                //n-gram level 3 process
 
+                Console.WriteLine("3 level n-gram");
+                Console.WriteLine();
+                using (StreamWriter writer = new StreamWriter("debug.txt", true))
+                {
+                    writer.WriteLine();
+                    writer.WriteLine("3 level n-gram");
+                    writer.WriteLine();
+                }
+                ngrams = reader.turnIntoNgram(3, words);
+                definitions = reader.getngram(ngrams, nIndexs, nData);
+                reader.print(definitions);
+                reader.writetofile(definitions);
+
+                //4 level ngram
+                Console.WriteLine();
+                Console.WriteLine("4 level n-gram");
+                Console.WriteLine();
+                using (StreamWriter writer = new StreamWriter("debug.txt", true))
+                {
+                    writer.WriteLine();
+                    writer.WriteLine("4 level n-gram");
+                    writer.WriteLine();
+                }
+                ngrams = reader.turnIntoNgram(4, words);
+                definitions = reader.getngram(ngrams, nIndexs, nData);
+                reader.print(definitions);
+                reader.writetofile(definitions);
+            }       
             Console.WriteLine("\n");
             Console.WriteLine("Thanks for using N-gram Extractor by Roberto Davies-Amaral and Sonia Friesen");
             using (StreamWriter writer = new StreamWriter("debug.txt", true))
